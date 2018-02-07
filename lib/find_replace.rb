2eae10e5a96d84318fac433b957058ddc9270dd8
Emily Watkins and Kimberly Huynh <emily@>
2018-02-07 14:46:08 -0800

    passing test, refactored method to replace words in string

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -3,10 +3,13 @@ class Document
     @string = string
   end
 
-  def find(locate, replace)
-    if @string.include? locate
-      return locate
+  def find(val_1, val_2)
+    new_string = @string
+    if new_string.include? val_1
+      new_string[val_1]= val_2
     end
+    new_string
   end
+
 
 end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -4,9 +4,14 @@ require('find_replace')
 
 describe('Document') do
   describe("#find") do
-    it("find a word in a two word sentence and returns that word") do
-      string = Document.new("hello world")
-      expect(string.find("hello", "bye")).to(eq("hello"))
+    # it("find a word in a two word sentence and returns that word") do
+    #   string = Document.new("hello world")
+    #   expect(string.find("hello", "bye")).to(eq("hello"))
+    # end
+
+    it("finds and replaces a word in a string") do
+      string = Document.new("I am walking my cat to the cathedral")
+      expect(string.find("cat", "dog")).to(eq("not sure"))
     end
   end
 
